de60aa35e0a7086b43ca3e3af24c6356077843b2
william mckee <william.mckee@techtorium.ac.nz>
2022-08-19 22:45:48 +1200

    start

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { writable } from 'svelte/store';
+    let date = new Date()
+
+
+export const user = writable({
+  firstname: "",
+  lastname: "",
+  gender: "",
+  email: "",
+  date: "",
+  
+})
+</script>
+
+<h1>Verified identity data</h1>
+<form class="content">
+  <label>First Name</label>
+  <input type="text" bind:value={$user.firstname} />
+  <label>Last Name</label>
+  <input type="text" bind:value={$user.lastname} />
+  <label for="gender" id="gender">Gender</label>
+  <select bind:value={$user.gender} name="gender" id="gender">
+    <option value="male">Male</option>
+    <option value="female">Female</option>
+  </select>
+  <label>Date of Birth</label>
+  <input type="date" bind:value={$user.date}>
+
+
+</form>
+<p>
+	{JSON.stringify($user, 0, 2)}</p>
+
---
+++ src/routes/form.svelte
@@ -0,0 +1,7 @@
+<h1>Verified identity data</h1>
+
+<code>
+    {"transactionId":"_823B8A5F8B55B38DC529EB2CE5907ECE","name":{"firstName":"Agency","middleName":"Data","lastName":"One"," ":false},
+"placeOfBirth":{"locality":"AUCKLAND","country":"New Zealand","placeOfBirthDisputed":false},
+"gender":{"genderValue":"M","genderDisputed":false},"dateOfBirth":{"dateOfBirthValue":"1981-11-11","dateOfBirthDisputed":false}}
+</code>
